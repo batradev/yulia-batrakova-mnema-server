64d2a0a8934dce2d4767a3f9e054d1577e5dd0a1
Yuliya Batrakova <batrakova.yulia@gmail.com>
2024-08-20 16:17:49 -0400

    Initial setup of Express server with basic routes

---
+++ src/app.js
@@ -0,0 +1,17 @@
+
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const apiRoutes = require('./routes/api'); 
+const app = express();
+
+
+app.use(cors());
+app.use(express.json());
+
+
+app.use('/api', apiRoutes);
+
+module.exports = app;
+
