c2e260500ff5a6e604775c0c47d7dcc31152ad63
Yuliya Batrakova <batrakova.yulia@gmail.com>
2024-09-18 20:58:16 -0400

    Fixing Google oauth and specifying URLs based on the ENV

--- src/config/passport.js
+++ src/config/passport.js
@@ -5,7 +5,7 @@ const db = require('../db');
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: '/auth/google/callback'
+    callbackURL: process.env.GOOGLE_CALLBACK_URL
   },
   async function(token, tokenSecret, profile, done) {
     try {
